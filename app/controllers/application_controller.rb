2ea5340b77f80c3b9ec6d63474bc91bfc527c274
immonroe <immanuelmonroe@gmail.com>
2024-08-19 14:28:46 -0400

    Added access buttons in nav for user state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def set_current_user
+    return unless session[:user_id]
+
+    Current.user = User.find_by(id: session[:user_id])
+  end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,2 @@
 class MainController < ApplicationController
-  def index
-    return unless session[:user_id]
-
-    @user = User.find_by(id: session[:user_id])
-    nil unless @user
-  end
 end
